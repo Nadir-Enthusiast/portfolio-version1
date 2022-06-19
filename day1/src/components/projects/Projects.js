24cc959d43d989e2f550ca41b6165413602a1108
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-19 00:03:23 -0400

    Create Projects.js

---
+++ day1/src/components/projects/Projects.js
@@ -0,0 +1,16 @@
+import "./Projects.css"
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className="projects">
+        <section id="projects">
+            <div class="container two-div">
+                <h1>TESTING</h1>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Projects
