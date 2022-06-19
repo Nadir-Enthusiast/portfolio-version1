8737a85f6b114831109a1dda74f8be1819b46280
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-18 23:59:16 -0400

    Create Contact.js

---
+++ day1/src/components/contact/Contact.js
@@ -0,0 +1,16 @@
+import "./Contact.css"
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="contact">
+      <section id="contact">
+            <div class="container three-div">
+                <h1>TESTING</h1>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Contact
