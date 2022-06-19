5d07c77d6e4af9af4d997be9a5c241c593850116
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-19 00:05:22 -0400

    Create Skills.js

---
+++ day1/src/components/skills/Skills.js
@@ -0,0 +1,16 @@
+import "./Skills.css"
+import React from 'react'
+
+function Skills() {
+  return (
+    <div className="container">
+        <section id="skills">
+            <div className="mainPage">
+                <h1>Testing</h1>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Skills
