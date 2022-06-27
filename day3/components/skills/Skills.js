dd7b1b84d69f962e55e3448ce9d0dda9bef8c348
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-27 15:17:48 -0400

    Create Skills.js

---
+++ day3/components/skills/Skills.js
@@ -0,0 +1,79 @@
+import "./Skills.css"
+import React from 'react'
+
+function Skills() {
+  return (
+    <div className="skills-page">
+        <section id="skills">
+            <div className="containers-skills">
+                <div className="backend-container">
+                  <div className="title-container-skill">
+                    <h1>Backend</h1>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>Node js</p>
+                    <svg >
+                      <line className="line-svg" x1="20" y1="0" x2="140" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>Express js</p>
+                    <svg >
+                      <line className="line-svg" x1="20" y1="0" x2="150" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>Firebase</p>
+                    <svg width="350px">
+                      <line className="line-svg" x1="20" y1="0" x2="350" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>Mongo DB</p>
+                    <svg >
+                      <line className="line-svg" x1="20" y1="0" x2="200" y2="0" />
+                    </svg>
+                  </div>
+                </div>
+                <div className="general-info">
+                  <div className="genInfo-text">
+                    <h1>Web Development Skills</h1>
+                    <p>Based on built projects and knowledge</p>
+                  </div>
+                </div>
+                <div className="frontend-container">
+                  <div className="title-container-skill">
+                    <h1>Frontend</h1>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>React js</p>
+                    <svg width="350px">
+                      <line className="line-svg" x1="20" y1="0" x2="320" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>Anime js</p>
+                    <svg >
+                      <line className="line-svg" x1="20" y1="0" x2="150" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>JS and HTML</p>
+                    <svg width="400px" >
+                      <line className="line-svg" x1="20" y1="0" x2="350" y2="0" />
+                    </svg>
+                  </div>
+                  <div className="skill-level-container">
+                    <p>CSS</p>
+                    <svg width="350px">
+                      <line className="line-svg" x1="20" y1="0" x2="300" y2="0" />
+                    </svg>
+                  </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Skills
